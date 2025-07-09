1adf94ce4c83f046b03a5efe93e6b2df9e80f313
Nr1cha <vrm.richards@gmail.com>
2025-07-08 19:47:08 -0600

    added directory and files for initial grid componant

---
+++ src/components/grid.jsx
@@ -0,0 +1,20 @@
+'use client';
+import '../grid.css'
+
+function CssGrid() {
+
+    return (
+        <>
+            <div className='contentGrid grid container'>
+                <div className="box">Box 1</div>
+                <div className="box">Box 2</div>
+                <div className="box">Box 3</div>
+                <div className="box">Box 4</div>
+                <div className="box">Box 5</div>
+                <div className="box">Box 6</div>
+            </div>
+        </>
+    )
+}
+
+export default CssGrid
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import './index.css'
+// import App from './App.jsx'
+import CssGrid from './components/grid.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <CssGrid />
   </StrictMode>,
 )
